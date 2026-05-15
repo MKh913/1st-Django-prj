3b772b958b7bb8c04e8f79e3de494f030028e1b1
Mahyar <mahyarkhodayari13@gmail.com>
2026-05-15 20:44:46 +0330

    Add Apps Posts
    (Database tables)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+    #author=models.CharField(max_length=25)
+    counted_views=models.IntegerField(default=0)
+    status=models.BooleanField(default=False) #Drafted OR Published
+
+    #image=models.CharField(max_length=100)
+    #category=models.CharField(max_length=25)
+    #tag=models.CharField(max_length=25)
+    
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
+    published_date=models.DateField(null=True)
